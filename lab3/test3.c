c38e599d775420854c0870bc0900bf78fbca3543
up201503256 <up201503256@ef086422-58f5-46ff-8ee3-c03d0d6e3c4f>
2017-10-16 08:01:56 +0000

    Printing hello message in each test3.c function

---
+++ lab3/test3.c
@@ -0,0 +1,9 @@
+int kbd_test_scan(unsigned short ass) {
+	printf("Hello I am kbd_test_scan()\n");
+}
+int kbd_test_poll() {
+	printf("Hello I am kbd_test_poll()\n");
+}
+int kbd_test_timed_scan(unsigned short n) {
+	printf("Hello I am kbd_test_timed_scan()\n");
+}
---
+++ lab3/test3.d
@@ -0,0 +1,2 @@
+
+test3.o:	test3.c
