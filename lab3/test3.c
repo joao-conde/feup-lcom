8f195667928a3c3f641e3122ae4af7b13f8dcafc
up201503256 <up201503256@ef086422-58f5-46ff-8ee3-c03d0d6e3c4f>
2017-10-16 13:03:24 +0000

    :-)

--- lab3/test3.c
+++
@@ -1,9 +0,0 @@
-int kbd_test_scan(unsigned short ass) {
-	printf("Hello I am kbd_test_scan()\n");
-}
-int kbd_test_poll() {
-	printf("Hello I am kbd_test_poll()\n");
-}
-int kbd_test_timed_scan(unsigned short n) {
-	printf("Hello I am kbd_test_timed_scan()\n");
-}
