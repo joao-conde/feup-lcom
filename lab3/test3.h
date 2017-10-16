7ab0035860dc82d31c49a6044f7430d1388e1d5f
up201503256 <up201503256@ef086422-58f5-46ff-8ee3-c03d0d6e3c4f>
2017-10-16 07:55:07 +0000

    lab3.c allows calling of test functions

--- lab3/test3.h
+++ lab3/test3.h
@@ -4,40 +4,39 @@
 /** @defgroup test3 test3
  * @{
  *
- * Functions for testing the i8042 KBC
+ * Functions for testing the kbd code
  */
 
-
-/**
- * @brief Tests printing the scancodes (both make and break codes) from the KBC
- *
- * Each key pressed from keyboad will have it's makecode and breakcode printed upon press and release.
- * Function can be executed in C or assembly code
- *
- * @param assembly Execute assembly or C code
+/** 
+ * @brief To test reading of scancode via KBD interrupts 
+ * 
+ * Displays the scancodes received from the keyboard
+ * Exits upon release of the ESC key
+ * 
+ * @param asm Which IH to use: if 0, in C, otherwise in assembly
+ * 
  * @return Return 0 upon success and non-zero otherwise
  */
-int kbd_test_scan(unsigned short assembly);
+int kbd_test_scan(unsigned short asm);
 
 /**
- * @brief Tests turning LED's on and off
- *
- * Process each element of the toggle array per second
- *
- * @param size Length of toggle array
- * @param *toggle Base of toggle array
+ * @brief To test reading of scancodes via polling
+ * 
+ * Displays the scancodes received from the keyboard
+ * Exits upon release of the ESC key
+ * 
  * @return Return 0 upon success and non-zero otherwise
  */
-int kbd_test_leds(unsigned short size, unsigned short *toggle);
+int kbd_test_poll();
 
-/**
- * @brief Test whether the program is able to handle interrupts from more than one device
- *
- * Similar to kbd_test_scan() in that it should print in the console the scancodes
- * Program should exit not only when the user releases the ESC key
- * but also if it does not receive a scancode for a number seconds equal to its argument, n.
- *
- * @param n Maximum number of seconds to wait if no code received
+/** 
+ * @brief To test handling of more than one interrupt
+ * 
+ *  Similar to kbd_test_scan() except that it 
+ *  should terminate also if no scancodes are received for n seconds
+ * 
+ * @param n Number of seconds without scancodes before exiting
+ * 
  * @return Return 0 upon success and non-zero otherwise
  */
 int kbd_test_timed_scan(unsigned short n);
