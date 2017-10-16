380e920cc89ed1c5e460144c3520fc1f6d00120f
João Dias Conde Azevedo <up201503256@fe.up.pt>
2017-10-16 15:05:58 +0000

    added initial test files

---
+++ lab3/test3.c
@@ -0,0 +1,9 @@
+int kbd_test_scan(unsigned short ass) {
+	/* To be completed */
+}
+int kbd_test_poll() {
+    /* To be completed */
+}
+int kbd_test_timed_scan(unsigned short n) {
+    /* To be completed */
+}
