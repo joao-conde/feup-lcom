3d25bc3944af544f35e72607f9178e0ca0c633ce
João Dias Conde Azevedo <up201503256@fe.up.pt>
2017-11-17 10:06:41 +0000

    updated lab5.c to call remaining functions

--- lab5/test5.d
+++ lab5/test5.d
@@ -4,6 +4,7 @@ test5.o:	/usr/include/sys/null.h
 test5.o:	/usr/include/sys/ucred.h
 test5.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include-fixed/ansi.h
 test5.o:	/usr/pkg/gcc44/lib/gcc/i686-pc-minix/4.4.3/include-fixed/unistd.h
+test5.o:	pixmap.h
 test5.o:	test5.c
 test5.o:	test5.h
 test5.o:	video_gr.h
