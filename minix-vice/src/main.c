e0ebada955aade10a9717e3380453edbef12d6d3
up201503256 <up201503256@ef086422-58f5-46ff-8ee3-c03d0d6e3c4f>
2017-11-26 13:50:07 +0000

    game object started

--- minix-vice/src/main.c
+++ minix-vice/src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char **argv){
 	printf("Start of MINIX-VICE!\n");
+
+	srand(time(NULL));
+	sef_startup();
+
+	vg_init(0x105);
+	drawSquare(0,0,10,10);
+	sleep(5);
+	vg_exit();
 	return 0;
 }
