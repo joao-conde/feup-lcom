de9530db9e89a9cab34c438d20a3d80bb4c101fd
João Dias Conde Azevedo <up201503256@fe.up.pt>
2017-11-27 14:32:45 +0000

    game object methods implemented

--- minix-vice/src/main.c
+++ minix-vice/src/main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <time.h>
 
-int main(int argc, char **argv){
+#include "MinixVice.h"
+
+int main(int argc, char **argv) {
 	printf("Start of MINIX-VICE!\n");
 
 	srand(time(NULL));
 	sef_startup();
 
 	vg_init(0x105);
-	drawSquare(0,0,10,10);
-	sleep(5);
+
+	MinixVice* game = (MinixVice*) malloc(sizeof(MinixVice));
+
+	while (!game->done) {
+
+		if (game->draw)
+			drawMinixVice(game);
+
+		updateMinixVice(game);
+	}
+
+	endMinixVice(game);
+
 	vg_exit();
 	return 0;
 }
