cf4a227fdd75fa6686c822298ef9821745c4ba7a
up201503256 <up201503256@ef086422-58f5-46ff-8ee3-c03d0d6e3c4f>
2017-11-30 12:57:54 +0000

    STB lib

--- minix-vice/src/stb_image_wrap.c
+++ minix-vice/src/stb_image_wrap.c
@@ -1,8 +1,21 @@
 #include "stb_image_wrap.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 //load PNG image file
-char* stb_loadPNG(int* width, int* height, char* path){
-	stbi_load(width, height, path);
+unsigned char* stb_loadPNG(char* path, int* width, int* height){
+
+	int* channels = (int*)malloc(sizeof(int));
+
+	unsigned char* pngImg = stbi_load(path, width, height, channels, 3);
+
+	if(pngImg == NULL){
+		printf("Failure loading PNG: %s", path);
+		return NULL;
+	}
+
+	return pngImg;
 }
 
 //free image
