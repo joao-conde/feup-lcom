b56d52cfe8e12675ec9218954207f02ba3f1ea5b
up201503256 <up201503256@ef086422-58f5-46ff-8ee3-c03d0d6e3c4f>
2017-12-19 18:14:02 +0000

    started rtc, added some bmps, fixed menu bmp

--- minix-vice/src/rtc.c
+++ minix-vice/src/rtc.c
@@ -1,4 +1,5 @@
 #include "rtc.h"
+#include <minix/drivers.h>
 
 
 //TODO check if rtc is updating, verifying the UIP in regA
@@ -7,11 +8,26 @@ int isRTCUpdating(){
 }
 
 //TODO develop a function to get the date from rtc
-void getDate(){
+void getDate(unsigned long *day, unsigned long *month, unsigned long *year){
+
+	sys_outb(0x70, 7);
+	sys_inb(0x71, day);
+	sys_outb(0x70, 8);
+	sys_inb(0x71, month);
+	sys_outb(0x70, 9);
+	sys_inb(0x71, year);
 
 }
 
 //TODO develop a function to get the hour from rtc
-void getHour(){
+void getHour(unsigned long *hour, unsigned long *minutes,
+		unsigned long *seconds){
+
+	sys_outb(0x70, 4);
+	sys_inb(0x71, hour);
+	sys_outb(0x70, 2);
+	sys_inb(0x71, minutes);
+	sys_outb(0x70, 0);
+	sys_inb(0x71, seconds);
 
 }
--- minix-vice/src/rtc.h
+++ minix-vice/src/rtc.h
@@ -6,10 +6,11 @@
 int isRTCUpdating();
 
 //TODO develop a function to get the date from rtc
-void getDate();
+void getDate(unsigned long *day, unsigned long *month, unsigned long *year);
 
 //TODO develop a function to get the hour from rtc
-void getHour();
+void getHour(unsigned long *hour, unsigned long *minutes,
+		unsigned long *seconds);
 
 
 
